38d792ce192c2e9f5ef0765165c124a85d2bdd31
Colin Nichols <colinnic@gmail.com>
2015-05-08 12:11:41 -0600

    updated alignment variables and class names to represent compass directions, for example align_content_sw

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ React.render(
 	<div>
 
 		<div className="bottom-illustration">
-			<DropButton label="hihi_bottom" layoutMode={DropButton.BOTTOM_ALIGN}>
+			<DropButton label="hihi_bottom" layoutMode={DropButton.ALIGN_CONTENT_NE}>
 				<DropTrigger ><div className="chevron">{" >"}</div> Hello from Below</DropTrigger>
 				<DropBoxContent>
 					When content is placed at the bottom of the page it would be nice to sense with Javascript.  This .rdd-drop-box has a min-width of 210px.
@@ -16,13 +16,23 @@ React.render(
 		</div>
 
 		<div className="right-illustration">
-			<DropButton label="hihi_right">
-				<DropTrigger ><div className="chevron">{" >"}</div> HI</DropTrigger>
+			<DropButton label="hihi_right" layoutMode={DropButton.ALIGN_CONTENT_SW}>
+				<DropTrigger ><div className="chevron">{"< "}</div> HI</DropTrigger>
 				<DropBoxContent>
 					When content is on the edge of the page it would be nice to sense with Javascript.
 				</DropBoxContent>
 			</DropButton>
 		</div>
+
+		<div className="bottomright-illustration">
+			<DropButton label="hihi_right" layoutMode={DropButton.ALIGN_CONTENT_NW}>
+				<DropTrigger ><div className="chevron">{"< "}</div> HI</DropTrigger>
+				<DropBoxContent>
+					When content is on the edge of the page it would be nice to sense with Javascript.
+				</DropBoxContent>
+			</DropButton>
+		</div>
+
 		<DropButton label="hihi">
 			<DropTrigger ><div className="chevron">{" >"}</div> Custom<span className="bold-text"> button content</span></DropTrigger>
 			<DropBoxContent>
